1967c595f854cd96ee384d15e17f46b1361c84d5
Oscar <oscar.altrogge@me.com>
2015-07-02 16:00:29 +0200

    Basic layout and functions for poll overview Also removed assets

--- client/app/polls/polls.controller.js
+++ client/app/polls/polls.controller.js
@@ -1,6 +1,46 @@
 'use strict';
 
 angular.module('voteMachineApp')
-  .controller('PollsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('PollsCtrl', function ($scope, $http, $state, socket, Auth) {
+    $scope.allPolls = [];
+
+    $http.get('/api/polls').success(function(allPolls) {
+      $scope.allPolls = allPolls;
+      socket.syncUpdates('poll', $scope.allPolls);
+    });
+
+    $scope.addPoll = function() {
+      if($scope.newPoll === '') {
+        return;
+      }
+
+      $http.post('/api/polls', {
+        title: $scope.newPoll,
+        question: $scope.newPoll+'?',
+        voteOptions: ['Yes', 'No'],
+        owner: Auth.getCurrentUser()._id
+      });
+
+      $scope.newPoll = '';
+    };
+
+    $scope.choosePoll = function(poll) {
+      $state.go('detail', {
+        pollId: poll._id
+      });
+    };
+
+    $scope.editPoll = function(poll) {
+      $state.go('edit', {
+        pollId: poll._id
+      });
+    };
+
+    $scope.deletePoll = function(poll) {
+      $http.delete('/api/polls/' + poll._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('poll');
+    });
   });
