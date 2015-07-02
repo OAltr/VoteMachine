11718b0c785756a198ea18c257d92931af31b4b5
Oscar <oscar.altrogge@me.com>
2015-07-02 16:58:31 +0200

    Basic layout and function for detail

--- client/app/polls/detail/detail.controller.js
+++ client/app/polls/detail/detail.controller.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('voteMachineApp')
-  .controller('DetailCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('DetailCtrl', function ($scope, $http, $state, Auth) {
+    $scope.poll = {
+      title: '42',
+      question: 'Is this even a real question?',
+      owner: '0',
+      voteOptions: ['Yes', 'No'],
+      answers: [{answer: 'Yes'},{answer:'No'},{answer:'Yes'}]
+    };
+
+    $http.get('/api/polls/'+$state.params.pollID).success(function(thePoll) {
+      $scope.poll = thePoll;
+    });
+
+    $scope.pollBelongsToUser = function(poll) {
+      return poll.owner === Auth.getCurrentUser()._id;
+    };
+
+    $scope.countVotes = function(voteOption) {
+      return $scope.poll.answers.filter(function(anAnswer) {
+          return anAnswer.answer === voteOption;
+        }).length;
+    };
   });
