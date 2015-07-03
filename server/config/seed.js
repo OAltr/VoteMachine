0839dc0b96a96612bf9754948870c2d27bcfe09e
Oscar <oscar.altrogge@me.com>
2015-07-03 09:27:32 +0200

    Added more data to seed

--- server/config/seed.js
+++ server/config/seed.js
@@ -20,70 +20,94 @@ var pollTwoID = new ObjectId();
 var pollThreeID = new ObjectId();
 
 User.find({}).remove(function() {
-  User.create({
-    _id: testUserID,
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    _id: adminUserID,
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function() {
-      console.log('finished populating users');
-  });
+	User.create({
+		_id: testUserID,
+		provider: 'local',
+		name: 'Test User',
+		email: 'test@test.com',
+		password: 'test'
+	}, {
+		_id: adminUserID,
+		provider: 'local',
+		role: 'admin',
+		name: 'Admin',
+		email: 'admin@admin.com',
+		password: 'admin'
+	}, function() {
+			console.log('finished populating users');
+	});
 });
 
 Poll.find({}).remove(function() {
-  Poll.create({
-    _id: pollOneID,
-    title: 'VoteMachine',
-    question: 'How would you rate this site?',
-    voteOptions: ['Perfect', 'Good', 'Okay', 'At least you tried', 'Worst site ever!!!'],
-    owner: adminUserID
-  }, {
-    _id: pollTwoID,
-    title: 'Fruits',
-    question: 'This is a poll about your favourite fruit.',
-    voteOptions: ['Banana', 'Apple', 'Chocolate'],
-    owner: testUserID
-  }, {
-    _id: pollThreeID,
-    title: '1+1',
-    question: 'The result of 1+1?',
-    voteOptions: ['2','11','10'],
-    owner: adminUserID
-  }, function() {
-    console.log('finished populating polls');
-  });
+	Poll.create({
+		_id: pollOneID,
+		title: 'VoteMachine',
+		question: 'How would you rate this site?',
+		voteOptions: ['Perfect', 'Good', 'Okay', 'At least you tried', 'Worst site ever!!!'],
+		owner: adminUserID
+	}, {
+		_id: pollTwoID,
+		title: 'Fruits',
+		question: 'This is a poll about your favourite fruit.',
+		voteOptions: ['Banana', 'Apple', 'Chocolate'],
+		owner: testUserID
+	}, {
+		_id: pollThreeID,
+		title: '1+1',
+		question: 'The result of 1+1?',
+		voteOptions: ['2','11','10'],
+		owner: adminUserID
+	}, function() {
+		console.log('finished populating polls');
+	});
 });
 
 Answer.find({}).remove(function() {
-  Answer.create({
-    answer: 'Okay',
-    voter: testUserID,
-    poll: pollOneID
-  }, {
-    answer: 'Good',
-    voter: adminUserID,
-    poll: pollOneID
-  }, {
-    answer: 'Banana',
-    voter: adminUserID,
-    poll: pollTwoID
-  }, {
-    answer: 'Orange',
-    voter: testUserID,
-    poll: pollTwoID
-  }, {
-    answer: '10',
-    voter: testUserID,
-    poll: pollThreeID
-  }, function() {
-    console.log('finished populating answers');
-  });
+	Answer.create({
+		answer: 'Perfect',
+		voter: new ObjectId(),
+		poll: pollOneID
+	}, {
+		answer: 'Okay',
+		voter: new ObjectId(),
+		poll: pollOneID
+	}, {
+		answer: 'Okay',
+		voter: new ObjectId(),
+		poll: pollOneID
+	}, {
+		answer: 'Okay',
+		voter: new ObjectId(),
+		poll: pollOneID
+	}, {
+		answer: 'Okay',
+		voter: new ObjectId(),
+		poll: pollOneID
+	}, {
+		answer: 'Worst site ever!!!',
+		voter: new ObjectId(),
+		poll: pollOneID
+	}, {
+		answer: 'Okay',
+		voter: testUserID,
+		poll: pollOneID
+	}, {
+		answer: 'Good',
+		voter: adminUserID,
+		poll: pollOneID
+	}, {
+		answer: 'Banana',
+		voter: adminUserID,
+		poll: pollTwoID
+	}, {
+		answer: 'Orange',
+		voter: testUserID,
+		poll: pollTwoID
+	}, {
+		answer: '10',
+		voter: testUserID,
+		poll: pollThreeID
+	}, function() {
+		console.log('finished populating answers');
+	});
 });
