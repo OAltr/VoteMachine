8211a557b53950e93bf50739f946ddea8bc2d8f6
Oscar <oscar.altrogge@me.com>
2015-07-04 19:51:24 +0200

    Creating a new poll takes you to the edit page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,38 +1,42 @@
 'use strict';
 
 angular.module('voteMachineApp')
-  .controller('MainCtrl', function ($scope, $http, $state, socket, Auth) {
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.awesomePolls = [];
+	.controller('MainCtrl', function ($scope, $http, $state, socket, Auth) {
+		$scope.isLoggedIn = Auth.isLoggedIn;
+		$scope.awesomePolls = [];
 
-    $http.get('/api/polls').success(function(awesomePolls) {
-      $scope.awesomePolls = awesomePolls;
-      socket.syncUpdates('poll', $scope.awesomePolls);
-    });
+		$http.get('/api/polls').success(function(awesomePolls) {
+			$scope.awesomePolls = awesomePolls;
+			socket.syncUpdates('poll', $scope.awesomePolls);
+		});
 
-    $scope.addPoll = function() {
-      if($scope.newPoll === '') {
-        return;
-      }
+		$scope.addPoll = function() {
+			if($scope.newPoll === '') {
+				return;
+			}
 
-      $http.post('/api/polls', {
-        title: $scope.newPoll,
-        question: $scope.newPoll+'?',
-        voteOptions: ['Yes', 'No'],
-        owner: Auth.getCurrentUser()._id
-      });
+			$http.post('/api/polls', {
+				title: $scope.newPoll,
+				question: $scope.newPoll+'?',
+				voteOptions: ['Yes', 'No'],
+				owner: Auth.getCurrentUser()._id
+			}).success(function(poll) {
+				$state.go('edit', {
+					pollID: poll._id
+				});
+			});
 
-      $scope.newPoll = '';
-    };
+			$scope.newPoll = '';
+		};
 
 
-    $scope.choosePoll = function(poll) {
-      $state.go('detail', {
-        pollID: poll._id
-      });
-    };
+		$scope.choosePoll = function(poll) {
+			$state.go('detail', {
+				pollID: poll._id
+			});
+		};
 
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('poll');
-    });
-  });
+		$scope.$on('$destroy', function () {
+			socket.unsyncUpdates('poll');
+		});
+	});
