712a477ef55ae1f63a0da7a35f71cad8fb2391e7
Oscar <oscar.altrogge@me.com>
2015-07-04 19:26:06 +0200

    Added the possibility to add and remove voteOptions

--- client/app/polls/edit/edit.controller.js
+++ client/app/polls/edit/edit.controller.js
@@ -1,57 +1,64 @@
 'use strict';
 
 angular.module('voteMachineApp')
-  .controller('EditCtrl', function ($scope, $http, $state, Auth) {
-    // TODO: redirect if poll dosen't belong to user
+	.controller('EditCtrl', function ($scope, $http, $state, Auth) {
+		// TODO: redirect if poll dosen't belong to user
+		$scope.poll = {
+			title: '42',
+			question: 'Is this even a real question?',
+			owner: '0',
+			voteOptions: ['Yes', 'No'],
+			answers: [{answer: 'Yes'},{answer:'No'},{answer:'Yes'}]
+		};
+		$scope.editPoll = JSON.parse(JSON.stringify($scope.poll));
+		$scope.newOption = '';
 
-    $scope.poll = {
-      title: '42',
-      question: 'Is this even a real question?',
-      owner: '0',
-      voteOptions: ['Yes', 'No'],
-      answers: [{answer: 'Yes'},{answer:'No'},{answer:'Yes'}]
-    };
+		$http.get('/api/polls/'+$state.params.pollID).success(function(thePoll) {
+			$scope.poll = thePoll;
+			$scope.editPoll = JSON.parse(JSON.stringify(thePoll));
+		});
 
-    $scope.editPoll = JSON.parse(JSON.stringify($scope.poll));
+		$scope.pollBelongsToUser = function(poll) {
+			return poll.owner === Auth.getCurrentUser()._id;
+		};
 
-    $http.get('/api/polls/'+$state.params.pollID).success(function(thePoll) {
-      $scope.poll = thePoll;
-      $scope.editPoll = JSON.parse(JSON.stringify(thePoll));
-    });
+		$scope.removeVoteOption = function(option) {
+			$scope.editPoll.voteOptions =  $scope.editPoll.voteOptions.filter(function(anOption) {
+				return anOption !== option;
+			});
+		};
 
-    $scope.pollBelongsToUser = function(poll) {
-      return poll.owner === Auth.getCurrentUser()._id;
-    };
+		$scope.addVoteOption = function(option) {
+			if(option === '') {
+				return;
+			}
 
-    $scope.countVotes = function(voteOption) {
-      return $scope.poll.answers.filter(function(anAnswer) {
-          return anAnswer.answer === voteOption;
-        }).length;
-    };
+			$scope.editPoll.voteOptions.push(option);
+			$scope.newOption = '';
+		};
 
-    // TODO: make voteOptions available to edit
+		$scope.savePoll = function(poll) {
+			if(poll.title === '' || poll.question === '' || poll.voteOptions.length < 2) {
+				return;
+			}
+			console.log(poll.voteOptions);
 
-    $scope.savePoll = function(poll) {
-      if(poll.title === '' || poll.question === '') {
-        return;
-      }
+			$http.patch('/api/polls/'+$state.params.pollID, {
+				title: poll.title,
+				question: poll.question,
+				voteOptions: poll.voteOptions
+			}).success(function(thePoll) {
+				$scope.poll = thePoll;
+				$scope.editPoll = JSON.parse(JSON.stringify(thePoll));
+			});
+		};
 
-      $http.patch('/api/polls/'+$state.params.pollID, {
-        title: poll.title,
-        question: poll.question,
-        voteOptions: poll.voteOption
-      }).success(function(thePoll) {
-        $scope.poll = thePoll;
-        $scope.editPoll = thePoll;
-      });
-    };
+		$scope.resetPoll = function() {
+			$scope.editPoll = JSON.parse(JSON.stringify($scope.poll));
+		};
 
-    $scope.resetPoll = function() {
-      $scope.editPoll = JSON.parse(JSON.stringify($scope.poll));
-    };
-
-    $scope.deletePoll = function(poll) {
-      $http.delete('/api/polls/' + poll._id);
-      $state.go('polls');
-    };
-  });
+		$scope.deletePoll = function(poll) {
+			$http.delete('/api/polls/' + poll._id);
+			$state.go('polls');
+		};
+	});
