b98764e105666fc6ed12257a407efad06149e410
Oscar <oscar.altrogge@me.com>
2015-07-08 16:56:42 +0200

    Added a confirmation to user delete

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,17 +1,21 @@
 'use strict';
 
 angular.module('voteMachineApp')
-  .controller('AdminCtrl', function ($scope, $http, Auth, User) {
+	.controller('AdminCtrl', function ($scope, $http, Auth, User, Modal) {
 
-    // Use the User $resource to fetch all users
-    $scope.users = User.query();
+		// Use the User $resource to fetch all users
+		$scope.users = User.query();
 
-    $scope.delete = function(user) {
-      User.remove({ id: user._id });
-      angular.forEach($scope.users, function(u, i) {
-        if (u === user) {
-          $scope.users.splice(i, 1);
-        }
-      });
-    };
-  });
+		$scope.delete = function(theUser) {
+			var deleteModal = Modal.confirm.delete(function(user) {
+				User.remove({ id: user._id });
+				angular.forEach($scope.users, function(u, i) {
+					if (u === user) {
+						$scope.users.splice(i, 1);
+					}
+				});
+			});
+
+			deleteModal(theUser.name, theUser);
+		};
+	});
